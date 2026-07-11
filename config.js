7689d2f02a1ba8f1a7aff32f8ff86fd77ce7f65b
khanaasi <khanaasif57828@gmail.com>
2026-07-11 15:04:53 +0530

    Add Supabase configuration to config.js
    Added configuration for Supabase URL and key.

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+    // Tumhari Di Hui Real Supabase Keys
+    SUPABASE_URL: "https://kvdfdflygyssnldvahzv.supabase.co",
+    SUPABASE_KEY: "sb_publishable_7MW440PYXtbHbJpAq1v-OQ__1KZvK4R"
+};
