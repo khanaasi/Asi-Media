cb4bd801208afca8af1551d6cb0f25ce0dcb1208
khanaasi <khanaasif57828@gmail.com>
2026-07-11 21:48:57 +0530

    Refactor Supabase configuration to use window object

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 // Asi Media - Supabase Database Configurations
-const SUPABASE_URL = "https://kvdfdflygyssnldvahzv.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_CrFqmZHxB__p9akDmKoaIQ_T0Lm9VnX";
+window.SUPABASE_URL = "https://kvdfdflygyssnldvahzv.supabase.co";
+window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_CrFqmZHxB__p9akDmKoaIQ_T0Lm9VnX";
 
-// Public Client (Sirf read karne ke liye)
-const supabasePublic = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
-
-// Note: Admin Secret Key code me nahi hai. First time Admin Panel me login karte waqt add karni hogi.
+// Public Client (Sirf read karne ke liye - Index.html ke liye)
+window.supabasePublic = window.supabase.createClient(window.SUPABASE_URL, window.SUPABASE_PUBLISHABLE_KEY);
